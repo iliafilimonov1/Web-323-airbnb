051c4d15a02b8b92d425f924b8413defa1e7fbd3
Ilia Filimonov <9991571155i@gmail.com>
2023-05-17 22:07:53 +0400

    медиа-запросы. адаптив карточек

--- src/js/help-details.js
+++ src/js/help-details.js
@@ -1,3 +1,12 @@
+/* skeleton */
+const allSkeletons = document.querySelectorAll('.skeleton');
+
+window.addEventListener('load', () => {
+  allSkeletons.forEach(element => {
+    element.classList.remove('skeleton');
+  })
+})
+
 /* модалка ask a question (flex) */
 const modal = document.querySelector('#modal-question'); // получение доступа к модалке
 const openModal = document.querySelector('.btn-ask'); // доступ к кнопке
